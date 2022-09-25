14d9ccd1da0a7fdb39e008af5b2a1f720b12d1df
shiftdev <miciaiasvieira@gmail.com>
2022-09-25 18:40:34 -0300

    define-end-game

--- project/script.js
+++ project/script.js
@@ -1,11 +1,22 @@
 const mario = document.querySelector(".mario")
+const pipe = document.querySelector(".pipe")
 
 function jump() {
     mario.classList.add("animation-mario-jump")
-
     setTimeout(() => {
         mario.classList.remove("animation-mario-jump")
     }, 500)
 }
 document.addEventListener("keydown", jump)
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft
+    if(pipePosition <= 100) {
+        pipe.style.animation = "none"
+        pipe.style.left = "100px"
+    }
+
+}, Infinity)
+
 
