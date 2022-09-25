188c9e82fdac81c50b6f8ef92fc8ae146abefc2f
shiftdev <miciaiasvieira@gmail.com>
2022-09-25 17:47:59 -0300

    create-function

--- project/script.js
+++ project/script.js
@@ -1,6 +1,8 @@
-const mario = document.querySelector(".mario")
+const mario = document.querySelector(".mario");
 
-mario.classList.add("animation-mario-jump")
+function jump() {
+    mario.classList.add("animation-mario-jump")
+}
 
-
+document.addEventListener("keydown", console.log("cli"))
 
